927e89b71af25a2b17b2032976a7bd640e989459
sobadname <sobadname@gmail.com>
2017-04-18 16:15:02 +0800

    init

---
+++ src/main/java/com/tykj/zuul/filter/AccessFilter.java
@@ -0,0 +1,44 @@
+package com.tykj.zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class AccessFilter extends ZuulFilter{
+	
+	private Logger logger = LoggerFactory.getLogger(ZuulFilter.class);
+
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("accessToken");
+        if(accessToken == null) {
+        	logger.warn("access token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("access token ok");
+        return null;
+	}
+
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
